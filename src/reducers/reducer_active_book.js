39b4569bf2e9ff6b3d6fbb745315c042f35b6fd3
Thiago Cifani <cifani.thiago@gmail.com>
2017-05-06 03:37:17 +0700

    Consuming actions with reducers

---
+++ src/reducers/reducer_active_book.js
@@ -0,0 +1,9 @@
+//State argument is local, not from application
+export default function(state = null, action) {
+  switch(action.type) {
+  case 'BOOK_SELECTED':
+    return action.payload;
+  }
+  
+  return state;
+}
